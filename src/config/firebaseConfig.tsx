51bbe4e93c002265a41cf6328265166d46318c15
Will Madden <w.a.madden@gmail.com>
2021-11-17 12:35:43 +0100

    Add Firebase config

---
+++ src/config/firebaseConfig.tsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByetaTs7zIjKucmKq44Q1eHxPkJS8vdjY",
+  authDomain: "spacebook-messenger.firebaseapp.com",
+  projectId: "spacebook-messenger",
+  storageBucket: "spacebook-messenger.appspot.com",
+  messagingSenderId: "548633682668",
+  appId: "1:548633682668:web:dfcbd68559b3942edb7bc9",
+} as const;
+
+export default firebaseConfig;
