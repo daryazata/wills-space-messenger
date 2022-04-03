67457976a39617c75d5e0dc5fb56017a42cdb1cb
Darya Zata <darya.zata@outlook.com>
2022-04-03 18:33:40 +0200

    feat: add display channel logic

--- src/pages/ChannelsPage.tsx
+++ src/pages/ChannelsPage.tsx
@@ -1,13 +1,50 @@
 import {
   IonContent,
   IonHeader,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
-} from "@ionic/react";
-import "./ChannelMessagesPage.css";
+  useIonViewDidEnter,
+} from '@ionic/react';
+import { DocumentData } from 'firebase/firestore';
+import { useState } from 'react';
+import { getChannels } from '../config/firebaseApp';
+import './ChannelMessagesPage.css';
+
+type ChannelsType = DocumentData[];
 
 const ChannelsPage: React.FC = () => {
+  const [channels, setChannels] = useState<ChannelsType>();
+
+  useIonViewDidEnter(() => {
+    getChannels()
+      .then((response) => {
+        setChannels(response);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  });
+
+  const displayChannels = (channels: ChannelsType) => {
+    return channels.map((channel) => {
+      return (
+        <IonItem
+          data-cy={`channel-${channel.id}`}
+          routerLink={`/channels/${channel.id}/messages`}
+          key={channel.id}
+        >
+          <IonLabel>{channel.name}</IonLabel>
+          <IonImg className='channels-image' src={channel.image}></IonImg>
+        </IonItem>
+      );
+    });
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,12 +53,12 @@ const ChannelsPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">Channels</IonTitle>
+            <IonTitle size='large'>Channels</IonTitle>
           </IonToolbar>
         </IonHeader>
-        🚫 <strong>OH NO! Fix me!</strong>
+        <IonList>{channels && displayChannels(channels)}</IonList>
       </IonContent>
     </IonPage>
   );
