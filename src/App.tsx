2159dd9549d1b53f82ce93eec5fe8c3e326f2268
Darya Zata <darya.zata@outlook.com>
2022-04-03 18:35:09 +0200

    chore: format app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,46 @@
-import { Route, Redirect } from "react-router-dom";
-import { IonApp, IonRouterOutlet } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import ChannelMessagesPage from "./pages/ChannelMessagesPage";
-import ChannelsPage from "./pages/ChannelsPage";
-import NotFoundPage from "./pages/NotFoundPage";
+import { Route, Redirect } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import ChannelMessagesPage from './pages/ChannelMessagesPage';
+import ChannelsPage from './pages/ChannelsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import "./theme/variables.css";
+import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route
-          exact
-          path="/channels/:channelId/messages"
-          component={ChannelMessagesPage}
-        />
-        <Route exact path="/channels" component={ChannelsPage} />
-        <Redirect exact from="/" to="/channels" />
-        <Route component={NotFoundPage} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route
+            exact
+            path='/channels/:channelId/messages'
+            component={ChannelMessagesPage}
+          />
+          <Route exact path='/channels' component={ChannelsPage} />
+          <Redirect exact from='/' to='/channels' />
+          <Route component={NotFoundPage} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
